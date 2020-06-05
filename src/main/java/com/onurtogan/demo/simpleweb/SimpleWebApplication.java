ac8dd4409acb7ea5de83c4d374134876c726d85c
otogan <onurtogan10@gmail.com>
2020-06-05 01:28:55 -0500

    initial project

---
+++ src/main/java/com/onurtogan/demo/simpleweb/SimpleWebApplication.java
@@ -0,0 +1,13 @@
+package com.onurtogan.demo.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurtogan/demo/simpleweb/SimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurtogan.demo.simpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
