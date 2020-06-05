90ed1667ef1224be595af00e128a587c7118f74f
otogan <onurtogan10@gmail.com>
2020-06-04 23:50:29 -0500

    initial package

---
+++ src/main/java/com/onurtogan/demo/simpledemo/SimpleSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.onurtogan.demo.simpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onurtogan/demo/simpledemo/SimpleSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurtogan.demo.simpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
