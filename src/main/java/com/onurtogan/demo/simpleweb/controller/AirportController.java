378d8686463d8c578ed1603665ea0856f6bbc8ad
otogan <onurtogan10@gmail.com>
2020-06-08 19:40:05 -0500

    add Greeting and modify controllers

--- src/main/java/com/onurtogan/demo/simpleweb/controller/AirportController.java
+++ src/main/java/com/onurtogan/demo/simpleweb/controller/AirportController.java
@@ -1,25 +1,37 @@
 package com.onurtogan.demo.simpleweb.controller;
 
 import com.onurtogan.demo.simpleweb.model.AirportInfo;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class AirportController {
 
     private List<AirportInfo> airportInfoList = new ArrayList<>();
+    private static final String SERVICE_WARNING_MESSAGE = "Berlin Schönefeld is closed for service today";
+
 
     public AirportController() {
+        airportInfoList.add(new AirportInfo("Kilimanjaro Arusha", "ARK", "HTAR"));
+        airportInfoList.add(new AirportInfo("Berlin Schönefeld", "TXL", "EDDT"));
+        airportInfoList.add(new AirportInfo("Delhi Indira Gandhi", "DEL", "VIDP"));
+        airportInfoList.add(new AirportInfo("Tokio Haneda", "HND", "RJTT"));
         airportInfoList.add(new AirportInfo("Berlin Teggel", "TXL", "EDDT"));
-        airportInfoList.add(new AirportInfo("Berlin Schönefeld", "SXF", "EDDB"));
     }
 
     @GetMapping("airports/{id}")
     public AirportInfo getAirportInfo(@PathVariable int id) {
-        return airportInfoList.get(id);
+        AirportInfo airportInfo = airportInfoList.get(id);
+        if (Objects.equals(airportInfo.getName(), "Berlin Schönefeld")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, SERVICE_WARNING_MESSAGE);
+        }
+        return airportInfo;
     }
 }
--- src/main/java/com/onurtogan/demo/simpleweb/controller/BotController.java
+++ src/main/java/com/onurtogan/demo/simpleweb/controller/BotController.java
@@ -1,14 +1,17 @@
 package com.onurtogan.demo.simpleweb.controller;
 
+import com.onurtogan.demo.simpleweb.model.Greeting;
 import com.onurtogan.demo.simpleweb.model.UserInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class BotController {
+
+    private static final String GREETING_TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
     @PostMapping(value = "/greet", consumes = "application/json")
     public String greet(@RequestBody UserInfo userInfo) {
@@ -21,5 +24,10 @@ public class BotController {
     public String greets(@RequestBody List<UserInfo> userInfoList) {
         // logging out users
         return String.format("OK, %d of users have been logged out!", userInfoList.size());
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(GREETING_TEMPLATE, name));
     }
 }
