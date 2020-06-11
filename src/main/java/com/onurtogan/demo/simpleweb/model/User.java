005a8f4dac3a9ffa172e9e38e15d2dcbb06c539f
otogan <onurtogan10@gmail.com>
2020-06-11 16:04:33 -0500

    setup database settings and create user model

---
+++ src/main/java/com/onurtogan/demo/simpleweb/model/User.java
@@ -0,0 +1,69 @@
+package com.onurtogan.demo.simpleweb.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "user")
+public class User {
+
+    @Id
+    @Column
+    private long userId;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean enabled;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return userId == user.userId;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (userId ^ (userId >>> 32));
+    }
+}
